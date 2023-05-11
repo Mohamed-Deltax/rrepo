6277191c27fca68399f6494c1d937437680c53a9
Mohamed Sameer <sameer@deltax.ai>
2023-05-11 18:21:27 +0900

    initial commit

---
+++ mages.py
@@ -0,0 +1,9 @@
+from simple_image_download import simple_image_download as simp
+
+responce = simp.simple_image_download
+
+keywords = ["johny depp jack", "jack sparrow"]
+
+for kw in keywords:
+    responce().download(kw, 30)
+    
